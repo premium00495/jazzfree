a08d64064f545990332fe7e665636b7b870c6323
premium00495 <premium00495@gmail.com>
2025-10-26 23:03:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// ---------- Channels Array ----------
+const channels = [
+  {name:"ARY Digital", 
+      category:"Drama", 
+      logo:"https://cdn.shortpixel.ai/spai/q_lossy+ret_img+to_webp/www.bizasialive.com/wp-content/uploads/2020/11/arydigital.jpg", 
+      stream:"https://cdn12isb.tamashaweb.com:8087/YlUHeDQb7a/ARYdigital-abr/playlist.m3u8"},
+
+  {name:"Green Entertainment", 
+      category:"Drama", 
+      logo:"https://yt3.ggpht.com/cG8Vd8MiTP-KEnpTdPmMfjUdAlQ1rimJvKSjpZs2v2aXjqU8TM_vmi1e9Mo3ZNKd9sm_hu4RJw=s800-c-k-c0x00ffffff-no-rj", 
+      stream:"https://cdn12isb.tamashaweb.com:8087/YlUHeDQb7a/Green_Entertainment-abr/playlist.m3u8"},
+
+  {name:"HUM TV", 
+      category:"Drama", 
+      logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRqNL_yQDDJ5X0Yx4vR9VA_lisDOtijReMnIpH6PsAqMtFFBaZAEcUp_Y&s=10", 
+      stream:"https://cdn05khi.tamashaweb.com:8087/YlUHeDQb7a/HUMTv-abr/playlist.m3u8"},
+
+  {name:"Geo News", 
+      category:"News", 
+      logo:"https://img.youtube.com/vi/ifgOMEerYQI/maxresdefault.jpg", 
+      stream:"https://cdn12isb.tamashaweb.com:8087/YlUHeDQb7a/geoNews-abr/playlist.m3u8"},
+
+  {name:"ARY News", 
+      category:"News", 
+      logo:"https://img.youtube.com/vi/RqUZ2Fv9l8w/maxresdefault.jpg", 
+      stream:"https://cdn23lhr.tamashaweb.com:8087/YlUHeDQb7a/ARYnews-abr/playlist.m3u8"},
+
+  {name:"Cartoon Network", 
+      category:"Cartoons", 
+      logo:"https://cdn.dribbble.com/userupload/29466057/file/original-592d1b655f23094e357161e8acec6a1f.gif", 
+      stream:"https://cdn05khi.tamashaweb.com:8087/YlUHeDQb7a/cartoonnetwork-abr/playlist.m3u8"},
+
+  {name:"Express Entertainment", 
+      category:"Entertainment", 
+      logo:"https://yt3.googleusercontent.com/BdINqhDsT1JfK17Osdn4UTVixJIaclMFmMwaBg18msbKVNraRgeX2P086bhFsCZOhWeq_DNUvQ=w2120-fcrop64=1,00000000ffffffff", 
+      stream:"https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"}
+];
